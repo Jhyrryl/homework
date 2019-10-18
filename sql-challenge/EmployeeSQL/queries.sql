2cb1ef611faf16f4b93f714657491d9bf7fee397
Mike Paddock <jhyrryl@gmail.com>
2019-10-17 18:50:55 -0700

    Refactor; updates; comments; more

--- sql-challenge/EmployeeSQL/queries.sql
+++ sql-challenge/EmployeeSQL/queries.sql
@@ -1,39 +1,55 @@
-select employees.emp_no, last_name, first_name, gender, salary
-from employees
-join salaries on
-employees.emp_no=salaries.emp_no;
+-- List the following details of each employee:
+-- 		employee number, last name, first name, gender, and salary.
+select employee.emp_no, last_name, first_name, gender, salary
+from employee
+join salary on
+employee.emp_no=salary.emp_no;
 
-select * from employees
+-- List employees who were hired in 1986.
+select * from employee
 where extract(year from hire_date)=1986;
 
-select dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, 
-	employees.last_name, employees.first_name, employees.hire_date, dept_manager.to_date
-from dept_manager
-left join employees on employees.emp_no=dept_manager.emp_no
-left join departments on departments.dept_no=dept_manager.dept_no
-where dept_manager.to_date='9999-01-01';
+-- List the manager of each department with the following information: 
+-- 		department number, department name, the manager's employee number, 
+-- 		last name, first name, and start and end employment dates.
+select departmentmanager.dept_no, department.dept_name, departmentmanager.emp_no, 
+	employee.last_name, employee.first_name, employee.hire_date, departmentmanager.to_date
+from departmentmanager
+left join employee on employee.emp_no=departmentmanager.emp_no
+left join department on department.dept_no=departmentmanager.dept_no
+where departmentmanager.to_date='9999-01-01';
 
-select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
-from dept_emp
-left join employees on employees.emp_no=dept_emp.emp_no
-left join departments on departments.dept_no=dept_emp.dept_no;
+-- List the department of each employee with the following information: 
+-- 		employee number, last name, first name, and department name.
+select employeedepartment.emp_no, employee.last_name, employee.first_name, department.dept_name
+from employeedepartment
+left join employee on employee.emp_no=employeedepartment.emp_no
+left join department on department.dept_no=employeedepartment.dept_no;
 
+-- List all employees whose first name is "Hercules" and last names begin with "B."
 select first_name, last_name
-from employees
+from employee
 where first_name='Hercules' and last_name like 'B%';
 
-select dept_emp.emp_no, last_name, first_name, departments.dept_name
-from dept_emp
-left join employees on dept_emp.emp_no=employees.emp_no
-left join departments on dept_emp.dept_no=departments.dept_no
-where dept_emp.dept_no='d007';
+-- List all employees in the Sales department, including their:
+-- 		employee number, last name, first name, and department name.
+select employeedepartment.emp_no, last_name, first_name, department.dept_name
+from employeedepartment
+left join employee on employeedepartment.emp_no=employee.emp_no
+left join department on employeedepartment.dept_no=department.dept_no
+where employeedepartment.dept_no='d007';
 
-select dept_emp.emp_no, last_name, first_name, departments.dept_name
-from dept_emp
-left join employees on dept_emp.emp_no=employees.emp_no
-left join departments on dept_emp.dept_no=departments.dept_no
-where dept_emp.dept_no='d007' or dept_emp.dept_no='d005';
+-- List all employees in the Sales and Development departments, including their:
+-- 		employee number, last name, first name, and department name.
+select employeedepartment.emp_no, last_name, first_name, department.dept_name
+from employeedepartment
+left join employee on employeedepartment.emp_no=employee.emp_no
+left join department on employeedepartment.dept_no=department.dept_no
+where employeedepartment.dept_no='d007' or employeedepartment.dept_no='d005';
 
-select last_name, count(last_name)
-from employees
-group by last_name;
+-- In descending order, list the frequency count of employee last names, 
+-- 		i.e., how many employees share each last name.
+select last_name, count(last_name) as freq_count
+from employee
+group by last_name
+order by freq_count desc;
--- sql-challenge/EmployeeSQL/schema.sql
+++ sql-challenge/EmployeeSQL/schema.sql
@@ -1,19 +1,21 @@
-DROP TABLE IF EXISTS titles;
-DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS departments;
+-- 1. Execute DROP and CREATE commands
+-- 2. Import csv files
+-- 3. EXECUTE ALTER commands
 
-CREATE TABLE departments
-(
+DROP TABLE IF EXISTS Title;
+DROP TABLE IF EXISTS Salary;
+DROP TABLE IF EXISTS DepartmentManager;
+DROP TABLE IF EXISTS EmployeeDepartment;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Department;
+
+CREATE TABLE Department(
   dept_no CHAR(4) NOT NULL UNIQUE,
   dept_name VARCHAR(20) NOT NULL,
   PRIMARY KEY (dept_no)
 );
 
-CREATE TABLE employees
-(
+CREATE TABLE Employee(
   emp_no INT NOT NULL UNIQUE,
   birth_date DATE NOT NULL,
   first_name VARCHAR(20) NOT NULL,
@@ -23,40 +25,54 @@ CREATE TABLE employees
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp
-(
+-- add primary key with SERIAL after data import
+CREATE TABLE EmployeeDepartment(
   emp_no INT NOT NULL,
   dept_no VARCHAR(4) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+  FOREIGN KEY (emp_no) REFERENCES Employee(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES Department(dept_no)
 );
 
-CREATE TABLE dept_manager
-(
+-- add primary key with SERIAL after data import
+CREATE TABLE DepartmentManager(
   dept_no VARCHAR(4) NOT NULL,
   emp_no INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  FOREIGN KEY (dept_no) REFERENCES Department(dept_no),
+  FOREIGN KEY (emp_no) REFERENCES Employee(emp_no)
 );
 
-CREATE TABLE salaries
-(
+-- add primary key with SERIAL after data import
+CREATE TABLE Salary(
   emp_no INT NOT NULL,
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  FOREIGN KEY (emp_no) REFERENCES Employee(emp_no)
 );
 
-CREATE TABLE titles
-(
+-- add primary key with SERIAL after data import
+CREATE TABLE Title(
   emp_no INT NOT NULL,
   title VARCHAR(30) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  FOREIGN KEY (emp_no) REFERENCES Employee(emp_no)
 );
+
+-- Only execute the following
+-- after csv files have been imported
+ALTER TABLE Title
+ADD id SERIAL;
+
+ALTER TABLE Salary
+ADD id SERIAL;
+
+ALTER TABLE EmployeeDepartment
+ADD id SERIAL;
+
+ALTER TABLE DepartmentManager
+ADD id SERIAL;
