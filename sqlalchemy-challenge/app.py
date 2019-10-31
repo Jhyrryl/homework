da186cc81cce84129735f9deee46908c057c9172
Mike Paddock <jhyrryl@gmail.com>
2019-10-31 09:32:58 -0700

    Index links work

--- sqlalchemy-challenge/app.py
+++ sqlalchemy-challenge/app.py
@@ -21,32 +21,33 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return (
-        "<a href='/api/v1.0/precipitation'>/api/v1.0/precipitation</a>"
-        "<a href='/api/v1.0/stations'>/api/v1.0/stations</a>,"
-        "<a href='<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a>",
-        "<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a>",
-        "/api/v1.0/<start>",
-        " - for example: <a href='/api/v1.0/2016-08-23'>/api/v1.0/2016-08-23</a>",
-        "/api/v1.0/<start>/<end>",
-        " - for example: <a href='/api/v1.0/2016-08-23/2017-08-23'>/api/v1.0/2016-08-23/2017-08-23</a>"
+        "<a href='/api/v1.0/precipitation' target='_blank'>/api/v1.0/precipitation</a><br/>"
+        "<a href='/api/v1.0/stations' target='_blank'>/api/v1.0/stations</a><br/>"
+        "<a href='/api/v1.0/tobs' target='_blank'>/api/v1.0/tobs</a><br/>"
+        "/api/v1.0/&lt;start&gt; (e.g., <a href='/api/v1.0/2016-08-23' target='_blank'>/api/v1.0/2016-08-23</a>)<br/>"
+        "/api/v1.0/&lt;start&gt;/&lt;end&gt; (e.g., <a href='/api/v1.0/2016-08-23/2017-08-23' target='_blank'>/api/v1.0/2016-08-23/2017-08-23</a>)"
     )
 
-@app.route("/api/v1.0/precipitation")
-def precipitation_v1_0():
-    session = Session(engine)
 
-@app.route("/api/v1.0/stations")
-def stations_v1_0():
-    session = Session(engine)
+# @app.route("/api/v1.0/precipitation")
+# def precipitation_v1_0():
+#     session = Session(engine)
 
-@app.route("/api/v1.0/tobs")
-def tobs_v1_0():
-    session = Session(engine)
+# @app.route("/api/v1.0/stations")
+# def stations_v1_0():
+#     session = Session(engine)
 
-@app.route("/api/v1.0/<start>")
-def start_v1_0():
-    session = Session(engine)
+# @app.route("/api/v1.0/tobs")
+# def tobs_v1_0():
+#     session = Session(engine)
 
-@app.route("/api/v1.0/<start>/<end>")
-def start_end_v1_0():
-    session = Session(engine)
+# @app.route("/api/v1.0/<start>")
+# def start_v1_0():
+#     session = Session(engine)
+
+# @app.route("/api/v1.0/<start>/<end>")
+# def start_end_v1_0():
+#     session = Session(engine)
+
+if __name__ == "__main__":
+    app.run(debug=True)
