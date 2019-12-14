224435a9758d12b644b4432430224535d029eae3
Mike Paddock <jhyrryl@gmail.com>
2019-12-14 01:28:37 -0800

    progress

--- plotly-challenge/Belly_Button_Diversity/static/js/app.js
+++ plotly-challenge/Belly_Button_Diversity/static/js/app.js
@@ -1,25 +1,36 @@
 function buildMetadata(sample) {
+    
+    // @TODO: Complete the following function that builds the metadata panel
 
-  // @TODO: Complete the following function that builds the metadata panel
+    // Use `d3.json` to fetch the metadata for a sample
+    d3.json(`./metadata/${sample}`).then((metadata) => {
+        // Use d3 to select the panel with id of `#sample-metadata`
+        md_panel = d3.select("#sample-metadata");
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+        // Use `.html("") to clear any existing metadata
+        md_panel.html("");
 
-    // Use `.html("") to clear any existing metadata
+        // Use `Object.entries` to add each key and value pair to the panel
+        // Hint: Inside the loop, you will need to use d3 to append new
+        // tags for each key-value in the metadata.
+        Object.entries(metadata).forEach((k, v) => {
+//            console.log(typeof(k))
+            md_panel.append("p").text(`${k[0]}: ${k[1]}`);
+        });
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+        // BONUS: Build the Gauge Chart
+        // buildGauge(data.WFREQ);
+    });    
 }
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+    // @TODO: Use `d3.json` to fetch the sample data for the plots
+    d3.json(`./samples/${sample}`).then(data => {
+    });
 
     // @TODO: Build a Bubble Chart using the sample data
+    
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
